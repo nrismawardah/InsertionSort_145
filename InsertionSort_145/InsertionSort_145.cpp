6031639148edebe6ed6030df0692fb4599ef2a0e
nrismawardah <nrismawardah@gmail.com>
2023-03-23 16:15:34 +0700

    memanggil variabel inputan n

--- InsertionSort_145/InsertionSort_145.cpp
+++ InsertionSort_145/InsertionSort_145.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int arr[20];        // Membuat array dengan panjang data 20
 int n;              // Membuat variable inputan n
 
+void input()
+{
+    cout << "Masukkan Jumlah Data pada Array :";    // Membuat inputan jumlah element Array
+    cin >> n;                                       // Memanggil variabel inputan n
+}
 int main()
 {
     std::cout << "Hello World!\n";
