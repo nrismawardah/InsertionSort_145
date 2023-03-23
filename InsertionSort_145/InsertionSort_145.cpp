7d9e7d805442663b6563156aaa70882029942c1f
nrismawardah <nrismawardah@gmail.com>
2023-03-23 18:48:35 +0700

    Membuat prosedur insertion sort

--- InsertionSort_145/InsertionSort_145.cpp
+++ InsertionSort_145/InsertionSort_145.cpp
@@ -36,11 +36,35 @@ void input()
 //Procedure Insertionsort
 void insertionsort()
 {
+
     int temp;       // Membuat variabel data temporer atau penyimpanan sementara
     int j;          // Membuat variabel j sebagai penanda
 
+    for (int i = 1; i < n; i++)             // 1. Looping dengan i dimulai dari 1 hingga n-1
+    {
 
-}
+        temp = arr[1];                      // 2. Simpan nilai arr[i] ke variabel sementara temp
+
+        j = i - 1;                          // 3. Setting nilai j sama dengan i-1;
+
+
+        while (j >= 0 && arr[j] > temp)     // 4. Looping while dimana nilai j lebih besar
+        {                                   // arr[j] lebih besar daripada temp
+            arr[j + 1] = arr[j];            // a. simpan arr[j] ke dalam variabel arr[j + 1]
+            j--;                            // b. Decrement nilai j by 1
+        }
+
+        arr[j + 1] = temp;                  // 5. Simpan nilai temp ke dalam arr[j + 1]
+
+
+        cout << endl;
+        cout << "\nPass " << i << ": ";     // Output ke layar
+        for (int k = 0; k < n; k++)
+        {                                   // Looping nilai k dimulai dari 0 hingga n-1
+            cout << arr[k] << " ";          // Output ke layar
+        }
+    }
+};
 
 int main()
 {
