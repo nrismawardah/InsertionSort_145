4392c00f419f878cd6af5dac9753d82b27757689
nrismawardah <nrismawardah@gmail.com>
2023-03-23 16:21:33 +0700

    Membuat kondisi n tidak lebih dari 20

--- InsertionSort_145/InsertionSort_145.cpp
+++ InsertionSort_145/InsertionSort_145.cpp
@@ -5,9 +5,18 @@ int arr[20];        // Membuat array dengan panjang data 20
 int n;              // Membuat variable inputan n
 
 void input()
-{
-    cout << "Masukkan Jumlah Data pada Array :";    // Membuat inputan jumlah element Array
-    cin >> n;                                       // Memanggil variabel inputan n
+{                   // Procedure input
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : ";   // Membuat inputan jumlah element Array
+        cin >> n;       // Memanggil variabel inputan n
+
+        if (n <= 20)
+        {               // Membuat kondisi n tidak lebih dari 20
+            break;
+        }
+    }
+    
 }
 int main()
 {
